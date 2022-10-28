a0aa107dfe8d9543ef00543f9606f3997d84fb7b
Shadow <shadic@msn.com>
2022-10-27 20:11:04 -0400

    CMXPatcher regional fix

--- CMXPatcher/BODYStructHandler.cs
+++ CMXPatcher/BODYStructHandler.cs
@@ -2,6 +2,7 @@
 using AquaModelLibrary.AquaMethods;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,30 +18,30 @@ namespace CMXPatcher
         {
             StringBuilder outText = new StringBuilder();
 
-            outText.AppendLine(type + " : " + body.body.id);
+            outText.AppendLine(type + " : " + body.body.id.ToString(new CultureInfo("en-US")));
 
-            outText.AppendLine("int_20 = " + body.body.int_20);
-            outText.AppendLine("int_24_0x9_0x9 = " + body.body2.int_24_0x9_0x9);
-            outText.AppendLine("int_28 = " + body.body2.int_28);
-            outText.AppendLine("int_2C = " + body.body2.int_2C);
+            outText.AppendLine("int_20 = " + body.body.int_20.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("int_24_0x9_0x9 = " + body.body2.int_24_0x9_0x9.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("int_28 = " + body.body2.int_28.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("int_2C = " + body.body2.int_2C.ToString(new CultureInfo("en-US")));
 
-            outText.AppendLine("costumeSoundId = " + body.body2.costumeSoundId);
-            outText.AppendLine("reference_id = " + body.body2.reference_id);
-            outText.AppendLine("int_38 = " + body.body2.int_38);
-            outText.AppendLine("int_3C = " + body.body2.int_3C);
+            outText.AppendLine("costumeSoundId = " + body.body2.costumeSoundId.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("reference_id = " + body.body2.reference_id.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("int_38 = " + body.body2.int_38.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("int_3C = " + body.body2.int_3C.ToString(new CultureInfo("en-US")));
 
-            outText.AppendLine("linkedInnerId = " + body.body2.linkedInnerId);
-            outText.AppendLine("int_44 = " + body.body2.int_44);
-            outText.AppendLine("legLength = " + body.body2.legLength);
-            outText.AppendLine("float_4C_0xB = " + body.body2.float_4C_0xB);
+            outText.AppendLine("linkedInnerId = " + body.body2.linkedInnerId.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("int_44 = " + body.body2.int_44.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("legLength = " + body.body2.legLength.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("float_4C_0xB = " + body.body2.float_4C_0xB.ToString(new CultureInfo("en-US")));
 
-            outText.AppendLine("float_50 = " + body.body2.float_50);
-            outText.AppendLine("float_54 = " + body.body2.float_54);
-            outText.AppendLine("float_58 = " + body.body2.float_58);
-            outText.AppendLine("float_5C = " + body.body2.float_5C);
+            outText.AppendLine("float_50 = " + body.body2.float_50.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("float_54 = " + body.body2.float_54.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("float_58 = " + body.body2.float_58.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("float_5C = " + body.body2.float_5C.ToString(new CultureInfo("en-US")));
 
-            outText.AppendLine("float_60 = " + body.body2.float_60);
-            outText.AppendLine("int_64 = " + body.body2.int_64);
+            outText.AppendLine("float_60 = " + body.body2.float_60.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("int_64 = " + body.body2.int_64.ToString(new CultureInfo("en-US")));
 
             return outText;
         }
@@ -56,63 +57,63 @@ namespace CMXPatcher
                 switch(contents[0])
                 {
                     case "int_20":
-                        body.body.int_20 = Int32.Parse(contents[1]);
+                        body.body.int_20 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "int_24_0x9_0x9":
-                        body.body2.int_24_0x9_0x9 = Int32.Parse(contents[1]);
+                        body.body2.int_24_0x9_0x9 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "int_28":
-                        body.body2.int_28 = Int32.Parse(contents[1]);
+                        body.body2.int_28 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "int_2C":
-                        body.body2.int_2C = Int32.Parse(contents[1]);
+                        body.body2.int_2C = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
 
                     case "costumeSoundId":
-                        body.body2.costumeSoundId = Int32.Parse(contents[1]);
+                        body.body2.costumeSoundId = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "reference_id":
-                        body.body2.reference_id = Int32.Parse(contents[1]);
+                        body.body2.reference_id = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "int_38":
-                        body.body2.int_38 = Int32.Parse(contents[1]);
+                        body.body2.int_38 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "int_3C":
-                        body.body2.int_3C = Int32.Parse(contents[1]);
+                        body.body2.int_3C = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
 
                     case "int_40":
                     case "linkedInnerId":
-                        body.body2.linkedInnerId = Int32.Parse(contents[1]);
+                        body.body2.linkedInnerId = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "int_44":
-                        body.body2.int_44 = Int32.Parse(contents[1]);
+                        body.body2.int_44 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "legLength":
-                        body.body2.legLength = Single.Parse(contents[1]);
+                        body.body2.legLength = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "float_4C_0xB":
-                        body.body2.float_4C_0xB = Single.Parse(contents[1]);
+                        body.body2.float_4C_0xB = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
 
                     case "float_50":
-                        body.body2.float_50 = Single.Parse(contents[1]);
+                        body.body2.float_50 = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "float_54":
-                        body.body2.float_54 = Single.Parse(contents[1]);
+                        body.body2.float_54 = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "float_58":
-                        body.body2.float_58 = Single.Parse(contents[1]);
+                        body.body2.float_58 = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "float_5C":
-                        body.body2.float_5C = Single.Parse(contents[1]);
+                        body.body2.float_5C = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
 
                     case "float_60":
-                        body.body2.float_60 = Single.Parse(contents[1]);
+                        body.body2.float_60 = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "int_64":
-                        body.body2.int_64 = Int32.Parse(contents[1]);
+                        body.body2.int_64 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
 
                     default:
--- CMXPatcher/HAIRStructHandler.cs
+++ CMXPatcher/HAIRStructHandler.cs
@@ -2,6 +2,7 @@
 using AquaModelLibrary.AquaMethods;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,51 +18,51 @@ namespace CMXPatcher
         {
             StringBuilder outText = new StringBuilder();
 
-            outText.AppendLine(type + " : " + hair.hair.id);
+            outText.AppendLine(type + " : " + hair.hair.id.ToString(new CultureInfo("en-US")));
 
-            outText.AppendLine("unkIntB1 = " + hair.hair.unkIntB1);
-            outText.AppendLine("unkInt1 = " + hair.hair.unkInt1);
-            outText.AppendLine("unkInt2 = " + hair.hair.unkInt2);
+            outText.AppendLine("unkIntB1 = " + hair.hair.unkIntB1.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt1 = " + hair.hair.unkInt1.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt2 = " + hair.hair.unkInt2.ToString(new CultureInfo("en-US")));
 
-            outText.AppendLine("unkFloat0 = " + hair.hair.unkFloat0);
-            outText.AppendLine("unkFloat1 = " + hair.hair.unkFloat1);
-            outText.AppendLine("unkFloat2 = " + hair.hair.unkFloat2);
-            outText.AppendLine("unkFloat3 = " + hair.hair.unkFloat3);
+            outText.AppendLine("unkFloat0 = " + hair.hair.unkFloat0.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkFloat1 = " + hair.hair.unkFloat1.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkFloat2 = " + hair.hair.unkFloat2.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkFloat3 = " + hair.hair.unkFloat3.ToString(new CultureInfo("en-US")));
 
-            outText.AppendLine("unkInt3 = " + hair.hair.unkInt3);
-            outText.AppendLine("unkInt4 = " + hair.hair.unkInt4);
-            outText.AppendLine("unkInt5 = " + hair.hair.unkInt5);
-            outText.AppendLine("unkInt6 = " + hair.hair.unkInt6);
+            outText.AppendLine("unkInt3 = " + hair.hair.unkInt3.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt4 = " + hair.hair.unkInt4.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt5 = " + hair.hair.unkInt5.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt6 = " + hair.hair.unkInt6.ToString(new CultureInfo("en-US")));
 
-            outText.AppendLine("unkInt7 = " + hair.hair.unkInt7);
-            outText.AppendLine("unkInt8 = " + hair.hair.unkInt8);
-            outText.AppendLine("unkFloat4 = " + hair.hair.unkFloat4);
-            outText.AppendLine("unkFloat5 = " + hair.hair.unkFloat5);
+            outText.AppendLine("unkInt7 = " + hair.hair.unkInt7.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt8 = " + hair.hair.unkInt8.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkFloat4 = " + hair.hair.unkFloat4.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkFloat5 = " + hair.hair.unkFloat5.ToString(new CultureInfo("en-US")));
 
-            outText.AppendLine("unkFloat6 = " + hair.hair.unkFloat6);
-            outText.AppendLine("unkInt9 = " + hair.hair.unkInt9);
-            outText.AppendLine("unkInt10 = " + hair.hair.unkInt10);
-            outText.AppendLine("unkInt11 = " + hair.hair.unkInt11);
+            outText.AppendLine("unkFloat6 = " + hair.hair.unkFloat6.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt9 = " + hair.hair.unkInt9.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt10 = " + hair.hair.unkInt10.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt11 = " + hair.hair.unkInt11.ToString(new CultureInfo("en-US")));
 
-            outText.AppendLine("unkInt12 = " + hair.hair.unkInt12);
-            outText.AppendLine("unkInt13 = " + hair.hair.unkInt13);
-            outText.AppendLine("unkInt14 = " + hair.hair.unkInt14);
-            outText.AppendLine("unkFloat7 = " + hair.hair.unkFloat7);
+            outText.AppendLine("unkInt12 = " + hair.hair.unkInt12.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt13 = " + hair.hair.unkInt13.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt14 = " + hair.hair.unkInt14.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkFloat7 = " + hair.hair.unkFloat7.ToString(new CultureInfo("en-US")));
 
-            outText.AppendLine("unkFloat8 = " + hair.hair.unkFloat8);
-            outText.AppendLine("unkFloat9 = " + hair.hair.unkFloat9);
-            outText.AppendLine("unkInt15 = " + hair.hair.unkInt15);
-            outText.AppendLine("unkInt16 = " + hair.hair.unkInt16);
+            outText.AppendLine("unkFloat8 = " + hair.hair.unkFloat8.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkFloat9 = " + hair.hair.unkFloat9.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt15 = " + hair.hair.unkInt15.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt16 = " + hair.hair.unkInt16.ToString(new CultureInfo("en-US")));
 
-            outText.AppendLine("unkInt17 = " + hair.hair.unkInt17);
-            outText.AppendLine("unkInt18 = " + hair.hair.unkInt18);
-            outText.AppendLine("unkInt19 = " + hair.hair.unkInt19);
-            outText.AppendLine("unkInt20 = " + hair.hair.unkInt20);
+            outText.AppendLine("unkInt17 = " + hair.hair.unkInt17.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt18 = " + hair.hair.unkInt18.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt19 = " + hair.hair.unkInt19.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkInt20 = " + hair.hair.unkInt20.ToString(new CultureInfo("en-US")));
 
-            outText.AppendLine("unkShortB1 = " + hair.hair.unkShortB1);
-            outText.AppendLine("unkShortB2 = " + hair.hair.unkShortB2);
-            outText.AppendLine("unkShortB3 = " + hair.hair.unkShortB3);
-            outText.AppendLine("unkShort0 = " + hair.hair.unkShort0);
+            outText.AppendLine("unkShortB1 = " + hair.hair.unkShortB1.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkShortB2 = " + hair.hair.unkShortB2.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkShortB3 = " + hair.hair.unkShortB3.ToString(new CultureInfo("en-US")));
+            outText.AppendLine("unkShort0 = " + hair.hair.unkShort0.ToString(new CultureInfo("en-US")));
 
             return outText;
         }
@@ -78,118 +79,118 @@ namespace CMXPatcher
                 switch(contents[0])
                 {
                     case "unkIntB1":
-                        hair.hair.unkIntB1 = Int32.Parse(contents[1]);
+                        hair.hair.unkIntB1 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt1":
-                        hair.hair.unkIntB1 = Int32.Parse(contents[1]);
+                        hair.hair.unkIntB1 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt2":
-                        hair.hair.unkIntB1 = Int32.Parse(contents[1]);
+                        hair.hair.unkIntB1 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
 
 
                     case "unkFloat0":
-                        hair.hair.unkFloat0 = Single.Parse(contents[1]);
+                        hair.hair.unkFloat0 = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkFloat1":
-                        hair.hair.unkFloat1 = Single.Parse(contents[1]);
+                        hair.hair.unkFloat1 = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkFloat2":
-                        hair.hair.unkFloat2 = Single.Parse(contents[1]);
+                        hair.hair.unkFloat2 = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkFloat3":
-                        hair.hair.unkFloat3 = Single.Parse(contents[1]);
+                        hair.hair.unkFloat3 = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
 
                     case "unkInt3":
-                        hair.hair.unkInt3 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt3 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt4":
-                        hair.hair.unkInt4 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt4 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt5":
-                        hair.hair.unkInt5 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt5 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt6":
-                        hair.hair.unkInt6 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt6 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
 
                     case "unkInt7":
-                        hair.hair.unkInt7 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt7 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt8":
-                        hair.hair.unkInt8 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt8 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkFloat4":
-                        hair.hair.unkFloat4 = Single.Parse(contents[1]);
+                        hair.hair.unkFloat4 = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkFloat5":
-                        hair.hair.unkFloat5 = Single.Parse(contents[1]);
+                        hair.hair.unkFloat5 = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
 
                     case "unkFloat6":
-                        hair.hair.unkFloat6 = Single.Parse(contents[1]);
+                        hair.hair.unkFloat6 = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt9":
-                        hair.hair.unkInt9 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt9 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt10":
-                        hair.hair.unkInt10 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt10 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt11":
-                        hair.hair.unkInt11 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt11 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
 
                     case "unkInt12":
-                        hair.hair.unkInt12 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt12 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt13":
-                        hair.hair.unkInt13 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt13 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt14":
-                        hair.hair.unkInt14 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt14 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkFloat7":
-                        hair.hair.unkFloat7 = Single.Parse(contents[1]);
+                        hair.hair.unkFloat7 = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
 
                     case "unkFloat8":
-                        hair.hair.unkFloat8 = Single.Parse(contents[1]);
+                        hair.hair.unkFloat8 = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkFloat9":
-                        hair.hair.unkFloat9 = Single.Parse(contents[1]);
+                        hair.hair.unkFloat9 = Single.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt15":
-                        hair.hair.unkInt15 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt15 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt16":
-                        hair.hair.unkInt16 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt16 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
 
                     case "unkInt17":
-                        hair.hair.unkInt17 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt17 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt18":
-                        hair.hair.unkInt18 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt18 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt19":
-                        hair.hair.unkInt19 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt19 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkInt20":
-                        hair.hair.unkInt20 = Int32.Parse(contents[1]);
+                        hair.hair.unkInt20 = Int32.Parse(contents[1], new CultureInfo("en-US"));
                         break;
 
                     case "unkShortB1":
-                        hair.hair.unkShortB1 = Int16.Parse(contents[1]);
+                        hair.hair.unkShortB1 = Int16.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkShortB2":
-                        hair.hair.unkShortB2 = Int16.Parse(contents[1]);
+                        hair.hair.unkShortB2 = Int16.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkShortB3":
-                        hair.hair.unkShortB3 = Int16.Parse(contents[1]);
+                        hair.hair.unkShortB3 = Int16.Parse(contents[1], new CultureInfo("en-US"));
                         break;
                     case "unkShort0":
-                        hair.hair.unkShort0 = Int16.Parse(contents[1]);
+                        hair.hair.unkShort0 = Int16.Parse(contents[1], new CultureInfo("en-US"));
                         break;
 
                     default:
