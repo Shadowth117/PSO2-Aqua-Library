90e37511a1140ad2b3b00be3d553d387bee2f946
Shadow <shadic@msn.com>
2023-02-19 00:47:30 -0500

    BluePointWip

--- AquaModelLibrary/BluePoint/CANI/CANI.cs
+++ AquaModelLibrary/BluePoint/CANI/CANI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Reloaded.Memory.Streams;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace AquaModelLibrary.BluePoint.CANI
 {
     public class CANI
     {
+        public CANIHeader header = null;
+        public CFooter footerData;
+
+        public CANI(BufferedStreamReader sr)
+        {
+            header = new CANIHeader(sr);
+            footerData = sr.Read<CFooter>();
+        }
     }
 }
