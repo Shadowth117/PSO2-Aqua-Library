3ad25e92fe3fab4ec489979bdf536ef965b63aa4
Shadow <shadic@msn.com>
2023-07-12 20:06:07 -0400

    CANI WIP

--- AquaModelLibrary/BluePoint/CANI/CANI.cs
+++ AquaModelLibrary/BluePoint/CANI/CANI.cs
@@ -10,11 +10,20 @@ namespace AquaModelLibrary.BluePoint.CANI
     public class CANI
     {
         public CANIHeader header = null;
+        public List<CANIFrameData> caniFrameData = new List<CANIFrameData>();
+        //public List<CANIMainData> caniMainData = new List<CANIMainData>();
         public CFooter footerData;
 
         public CANI(BufferedStreamReader sr)
         {
             header = new CANIHeader(sr);
+            foreach(var set in header.info)
+            {
+                sr.Seek(set.frameDataSetPointer, System.IO.SeekOrigin.Begin);
+                caniFrameData.Add(new CANIFrameData(sr));
+
+                //Large data
+            }
             footerData = sr.Read<CFooter>();
         }
     }
