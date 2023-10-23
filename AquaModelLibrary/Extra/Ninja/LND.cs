1208c77aa811cb4636da3c16127a657e0913325e
Shadow <shadic@msn.com>
2023-10-23 02:31:06 -0400

    ARC, LND WIP

---
+++ AquaModelLibrary/Extra/Ninja/LND.cs
@@ -0,0 +1,36 @@
+﻿using Reloaded.Memory.Streams;
+using System.Collections.Generic;
+
+namespace AquaModelLibrary.Extra.Ninja
+{
+    public class LND
+    {
+        public LND() { }
+
+        public LND(BufferedStreamReader sr)
+        {
+            BigEndianHelper._active = true;
+            var magicTest = sr.ReadBytes(0, 3);
+
+            if (magicTest[0] == 0x4C && magicTest[1] == 0x4E && magicTest[0] == 0x44)
+            {
+                ReadAltLND(sr);
+            } else {
+                ReadLND(sr);
+            }
+        }
+
+        /// <summary>
+        /// Specifically for 
+        /// </summary>
+        public void ReadAltLND(BufferedStreamReader sr)
+        {
+
+        }
+
+        public void ReadLND(BufferedStreamReader sr)
+        {
+
+        }
+    }
+}
