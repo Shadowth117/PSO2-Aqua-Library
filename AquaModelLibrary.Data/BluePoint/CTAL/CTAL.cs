06fa66b42439479eefc17e33fd9319452fa9c59e
Shadow <shadic@msn.com>
2024-04-18 16:39:14 -0400

    Add BluePoint Texture Atlas

---
+++ AquaModelLibrary.Data/BluePoint/CTAL/CTAL.cs
@@ -0,0 +1,41 @@
+﻿using AquaModelLibrary.Helpers.Readers;
+using System.Text;
+
+namespace AquaModelLibrary.Data.BluePoint.CTAL
+{
+    /// <summary>
+    /// BluePoint Texture Atlas
+    /// </summary>
+    public class CTAL
+    {
+        public string ctxrReference;
+        public List<CTALEntry> entries = new List<CTALEntry>();
+        public CFooter footer;
+
+        public CTAL() { }
+
+        public CTAL(byte[] file)
+        {
+            file = CompressionHandler.CheckCompression(file);
+            using (MemoryStream ms = new MemoryStream(file))
+            using (BufferedStreamReaderBE<MemoryStream> sr = new BufferedStreamReaderBE<MemoryStream>(ms))
+            {
+                Read(sr);
+            }
+        }
+
+        private void Read(BufferedStreamReaderBE<MemoryStream> sr)
+        {
+            byte strLength = sr.ReadBE<byte>();
+            ctxrReference = Encoding.UTF8.GetString(sr.ReadBytes(sr.Position, strLength));
+            sr.Seek(strLength, SeekOrigin.Current);
+            var entryCount = sr.ReadBE<int>();
+            
+            for(int i = 0; i < entryCount; i++)
+            {
+                entries.Add(sr.Read<CTALEntry>());
+            }
+            footer = sr.Read<CFooter>();
+        }
+    }
+}
---
+++ AquaModelLibrary.Data/BluePoint/CTAL/CTALEntry.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+namespace AquaModelLibrary.Data.BluePoint.CTAL
+{
+    public struct CTALEntry
+    {
+        public int hash;
+        public Vector2 upperLeftPoint;
+        public Vector2 lowerLeftPoint;
+        public Vector2 lowerRightPoint;
+        public Vector2 upperRightPoint;
+        public int unk0;
+    }
+}
