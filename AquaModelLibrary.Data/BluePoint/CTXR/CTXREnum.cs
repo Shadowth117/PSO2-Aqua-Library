aa084277fb80fa8858f20738dda2469165e1afe3
Shadow <shadic@msn.com>
2024-04-24 00:55:08 -0400

    Add texture type enum

---
+++ AquaModelLibrary.Data/BluePoint/CTXR/CTXREnum.cs
@@ -0,0 +1,10 @@
+﻿namespace AquaModelLibrary.Data.BluePoint.CTXR
+{
+    public enum CTextureType : int
+    {
+        Standard = 0,
+        CubeMap = 1,
+        Volume = 2,
+        LargeUI = 8, //Texture atlas? Only seen in Demon's Souls Remake for VERY large GUI textures or fonts
+    }
+}
