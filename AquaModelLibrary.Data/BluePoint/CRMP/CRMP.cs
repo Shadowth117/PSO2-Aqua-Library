b2880c3d753508be2b9d1b77be4e9e3dd00fa2c9
Shadow <shadic@msn.com>
2024-04-28 18:36:02 -0400

    Add Reference MaP parsing

---
+++ AquaModelLibrary.Data/BluePoint/CRMP/CRMP.cs
@@ -0,0 +1,37 @@
+﻿using AquaModelLibrary.Data.BluePoint.CRMP;
+using AquaModelLibrary.Helpers.Readers;
+
+namespace AquaModelLibrary.Data.BluePoint.CTAL
+{
+    /// <summary>
+    /// BluePoint Reference MaP
+    /// </summary>
+    public class CRMP
+    {
+        public List<CRMPEntry> entries = new List<CRMPEntry>();
+        public CFooter footer;
+
+        public CRMP() { }
+
+        public CRMP(byte[] file)
+        {
+            file = CompressionHandler.CheckCompression(file);
+            using (MemoryStream ms = new MemoryStream(file))
+            using (BufferedStreamReaderBE<MemoryStream> sr = new BufferedStreamReaderBE<MemoryStream>(ms))
+            {
+                Read(sr);
+            }
+        }
+
+        private void Read(BufferedStreamReaderBE<MemoryStream> sr)
+        {
+            int entryCount = sr.ReadBE<int>();
+
+            for (int i = 0; i < entryCount; i++)
+            {
+                entries.Add(new CRMPEntry(sr));
+            }
+            footer = sr.Read<CFooter>();
+        }
+    }
+}
---
+++ AquaModelLibrary.Data/BluePoint/CRMP/CRMPEntry.cs
@@ -0,0 +1,23 @@
+﻿using AquaModelLibrary.Helpers.Readers;
+using System.Text;
+
+namespace AquaModelLibrary.Data.BluePoint.CRMP
+{
+    public class CRMPEntry
+    {
+        public int idHash = -1;
+        public string reference;
+
+        public CRMPEntry() { }
+
+        public CRMPEntry(BufferedStreamReaderBE<MemoryStream> sr) 
+        {
+            idHash = sr.ReadBE<int>();
+            int refOffset = sr.ReadBE<int>();
+            var position = sr.Position;
+            sr.Seek(refOffset - 0x4, SeekOrigin.Current);
+            reference = sr.ReadCString();
+            sr.Seek(position, SeekOrigin.Begin);
+        }
+    }
+}
