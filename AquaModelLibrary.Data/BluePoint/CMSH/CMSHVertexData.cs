af87787c70c49c8a9ce9d85673cc9f3bb75d8c00
Shadow <shadic@msn.com>
2024-05-31 22:01:49 -0400

    Minor Refactor on CMSHVertData

--- AquaModelLibrary.Data/BluePoint/CMSH/CMSHVertexData.cs
+++ AquaModelLibrary.Data/BluePoint/CMSH/CMSHVertexData.cs
@@ -111,120 +111,124 @@ namespace AquaModelLibrary.Data.BluePoint.CMSH
             for (int i = 0; i < vertDefinitionsCount; i++)
             {
                 sr.Seek(vertexDataStart + vertDefs[i].dataStart, System.IO.SeekOrigin.Begin);
-                switch (vertDefs[i].dataMagic)
-                {
-                    case VertexMagic.POS0:
-                        for (int v = 0; v < vertCount; v++)
-                        {
-                            positionList.Add(sr.Read<Vector3>());
-                        }
-                        break;
-                    case VertexMagic.NRM0:
-                    case VertexMagic.TAN0:
-                    case VertexMagic.QUT0:
-                        for (int v = 0; v < vertCount; v++)
-                        {
-                            /*
-                            var byteArr = sr.ReadBytes(sr.Position(), 4);
-                            sr.Seek(4, System.IO.SeekOrigin.Current);
-                            Quaternion quat = new Quaternion(ConvertBPSbyte(byteArr[0]), ConvertBPSbyte(byteArr[1]), ConvertBPSbyte(byteArr[2]), ConvertBPSbyte(byteArr[3]));
-                            normalTemp.Add(byteArr);
-                            */
-                            var vector = sr.Read<int>();
+                ReadVertDefData(sr, vertCount, vertDefs[i].dataMagic, vertDefs[i].dataSize);
+                sr.Seek(vertexDataStart + vertDefs[i].dataStart + vertDefs[i].dataSize, System.IO.SeekOrigin.Begin);
+            }
+        }
+
+        public void ReadVertDefData(BufferedStreamReaderBE<MemoryStream> sr, int vertCount, VertexMagic dataMagic, long dataSize)
+        {
+            switch (dataMagic)
+            {
+                case VertexMagic.POS0:
+                    for (int v = 0; v < vertCount; v++)
+                    {
+                        positionList.Add(sr.Read<Vector3>());
+                    }
+                    break;
+                case VertexMagic.NRM0:
+                case VertexMagic.TAN0:
+                case VertexMagic.QUT0:
+                    for (int v = 0; v < vertCount; v++)
+                    {
+                        /*
+                        var byteArr = sr.ReadBytes(sr.Position(), 4);
+                        sr.Seek(4, System.IO.SeekOrigin.Current);
+                        Quaternion quat = new Quaternion(ConvertBPSbyte(byteArr[0]), ConvertBPSbyte(byteArr[1]), ConvertBPSbyte(byteArr[2]), ConvertBPSbyte(byteArr[3]));
+                        normalTemp.Add(byteArr);
+                        */
+                        var vector = sr.Read<int>();
 #if DEBUG
-                            var a = BitConverter.GetBytes(vector);
-                            normalsTesting.Add(a);
-                            normalsTesting2.Add($"{a[0]:X2}{a[1]:X2}{a[2]:X2}{a[3]:X2}");
+                        var a = BitConverter.GetBytes(vector);
+                        normalsTesting.Add(a);
+                        normalsTesting2.Add($"{a[0]:X2}{a[1]:X2}{a[2]:X2}{a[3]:X2}");
 #endif
-                            //var vector = BitConverter.ToInt32(byteArr, 0);
-                            int x = vector << 21 >> 21;
-                            int y = vector << 10 >> 21;
-                            int z = vector << 0 >> 22;
-                            var processedVectorTest = new Vector3(x / (float)0b11_1111_1111, y / (float)0b11_1111_1111, z / (float)0b1_1111_1111);
-                            var squareMagnitude = processedVectorTest.X * processedVectorTest.X + processedVectorTest.Y * processedVectorTest.Y + processedVectorTest.Z * processedVectorTest.Z;
-                            var scalar = 2.0 / (squareMagnitude + 1.0);
-                            var vector4 = new Vector4((float)(scalar * processedVectorTest.X), (float)(scalar * processedVectorTest.Y), (float)(scalar * processedVectorTest.Z), (float)((1.0 - squareMagnitude) / (1.0 + squareMagnitude)));
-                            Quaternion quatNormal = new Quaternion((float)(scalar * processedVectorTest.X), (float)(scalar * processedVectorTest.Y), (float)(scalar * processedVectorTest.Z), (float)((1.0 - squareMagnitude) / (1.0 + squareMagnitude)));
-                            normals.Add(quatNormal);
-                            //Debug.WriteLine($"Byte represntation {byteArr[0]:X2} {byteArr[1]:X2} {byteArr[2]:X2} {byteArr[3]:X2} - {((float)byteArr[0]) / 255} {((float)byteArr[1]) / 255} {((float)byteArr[2]) / 255} {((float)byteArr[3]) / 255} \nSByte representation {sbyteArr[0]:X2} {sbyteArr[1]:X2} {sbyteArr[2]:X2} {sbyteArr[3]:X2} - {((float)sbyteArr[0]) / 127} {((float)sbyteArr[1]) / 127} {((float)sbyteArr[2]) / 127} {((float)sbyteArr[3]) / 127} ");
-                            //Quaternion quat = new Quaternion( (float)(((double)sr.Read<sbyte>()) / 127), (float)(((double)sr.Read<sbyte>()) / 127), (float)(((double)sr.Read<sbyte>()) / 127), (float)(((double)sr.Read<sbyte>()) / 127));
-                            //normals.Add(quat);
-                        }
-                        break;
-                    case VertexMagic.COL0:
+                        //var vector = BitConverter.ToInt32(byteArr, 0);
+                        int x = vector << 21 >> 21;
+                        int y = vector << 10 >> 21;
+                        int z = vector << 0 >> 22;
+                        var processedVectorTest = new Vector3(x / (float)0b11_1111_1111, y / (float)0b11_1111_1111, z / (float)0b1_1111_1111);
+                        var squareMagnitude = processedVectorTest.X * processedVectorTest.X + processedVectorTest.Y * processedVectorTest.Y + processedVectorTest.Z * processedVectorTest.Z;
+                        var scalar = 2.0 / (squareMagnitude + 1.0);
+                        var vector4 = new Vector4((float)(scalar * processedVectorTest.X), (float)(scalar * processedVectorTest.Y), (float)(scalar * processedVectorTest.Z), (float)((1.0 - squareMagnitude) / (1.0 + squareMagnitude)));
+                        Quaternion quatNormal = new Quaternion((float)(scalar * processedVectorTest.X), (float)(scalar * processedVectorTest.Y), (float)(scalar * processedVectorTest.Z), (float)((1.0 - squareMagnitude) / (1.0 + squareMagnitude)));
+                        normals.Add(quatNormal);
+                        //Debug.WriteLine($"Byte represntation {byteArr[0]:X2} {byteArr[1]:X2} {byteArr[2]:X2} {byteArr[3]:X2} - {((float)byteArr[0]) / 255} {((float)byteArr[1]) / 255} {((float)byteArr[2]) / 255} {((float)byteArr[3]) / 255} \nSByte representation {sbyteArr[0]:X2} {sbyteArr[1]:X2} {sbyteArr[2]:X2} {sbyteArr[3]:X2} - {((float)sbyteArr[0]) / 127} {((float)sbyteArr[1]) / 127} {((float)sbyteArr[2]) / 127} {((float)sbyteArr[3]) / 127} ");
+                        //Quaternion quat = new Quaternion( (float)(((double)sr.Read<sbyte>()) / 127), (float)(((double)sr.Read<sbyte>()) / 127), (float)(((double)sr.Read<sbyte>()) / 127), (float)(((double)sr.Read<sbyte>()) / 127));
+                        //normals.Add(quat);
+                    }
+                    break;
+                case VertexMagic.COL0:
+                    for (int v = 0; v < vertCount; v++)
+                    {
+                        colors.Add(sr.ReadBytes(sr.Position, 4));
+                        sr.Seek(4, System.IO.SeekOrigin.Current);
+                    }
+                    break;
+                case VertexMagic.COL1:
+                    for (int v = 0; v < vertCount; v++)
+                    {
+                        color2s.Add(sr.ReadBytes(sr.Position, 4));
+                        sr.Seek(4, System.IO.SeekOrigin.Current);
+                    }
+                    break;
+                case VertexMagic.COL2:
+                    for (int v = 0; v < vertCount; v++)
+                    {
+                        color3s.Add(sr.ReadBytes(sr.Position, 4));
+                        sr.Seek(4, System.IO.SeekOrigin.Current);
+                    }
+                    break;
+                case VertexMagic.TEX0:
+                case VertexMagic.TEX1:
+                case VertexMagic.TEX2:
+                case VertexMagic.TEX3:
+                case VertexMagic.TEX4:
+                case VertexMagic.TEX5:
+                case VertexMagic.TEX6:
+                case VertexMagic.TEX7:
+                case VertexMagic.TEX8:
+                    var uvList = new List<Vector2>();
+                    for (int v = 0; v < vertCount; v++)
+                    {
+                        uvList.Add(new Vector2(sr.Read<Half>(), sr.Read<Half>()));
+                    }
+                    uvDict.Add(dataMagic, uvList);
+                    break;
+                case VertexMagic.BONI:
+                    var smolCount = dataSize / 0x4;
+                    if (vertCount != smolCount)
+                    {
                         for (int v = 0; v < vertCount; v++)
                         {
-                            colors.Add(sr.ReadBytes(sr.Position, 4));
+                            vertWeightIndices.Add(new int[] { sr.Read<ushort>(), sr.Read<ushort>(), sr.Read<ushort>(), sr.Read<ushort>() });
+                        }
+                    }
+                    else
+                    {
+                        for (int v = 0; v < vertCount; v++)
+                        {
+                            var indices = sr.ReadBytes(sr.Position, 4);
+                            vertWeightIndices.Add(new int[] { indices[0], indices[1], indices[2], indices[3] });
                             sr.Seek(4, System.IO.SeekOrigin.Current);
                         }
-                        break;
-                    case VertexMagic.COL1:
-                        for (int v = 0; v < vertCount; v++)
-                        {
-                            color2s.Add(sr.ReadBytes(sr.Position, 4));
-                            sr.Seek(4, System.IO.SeekOrigin.Current);
-                        }
-                        break;
-                    case VertexMagic.COL2:
-                        for (int v = 0; v < vertCount; v++)
-                        {
-                            color3s.Add(sr.ReadBytes(sr.Position, 4));
-                            sr.Seek(4, System.IO.SeekOrigin.Current);
-                        }
-                        break;
-                    case VertexMagic.TEX0:
-                    case VertexMagic.TEX1:
-                    case VertexMagic.TEX2:
-                    case VertexMagic.TEX3:
-                    case VertexMagic.TEX4:
-                    case VertexMagic.TEX5:
-                    case VertexMagic.TEX6:
-                    case VertexMagic.TEX7:
-                    case VertexMagic.TEX8:
-                        var uvList = new List<Vector2>();
-                        for (int v = 0; v < vertCount; v++)
-                        {
-                            uvList.Add(new Vector2(sr.Read<Half>(), sr.Read<Half>()));
-                        }
-                        uvDict.Add(vertDefs[i].dataMagic, uvList);
-                        break;
-                    case VertexMagic.BONI:
-                        var smolCount = vertDefs[i].dataSize / 0x4;
-                        if (vertCount != smolCount)
-                        {
-                            for (int v = 0; v < vertCount; v++)
-                            {
-                                vertWeightIndices.Add(new int[] { sr.Read<ushort>(), sr.Read<ushort>(), sr.Read<ushort>(), sr.Read<ushort>() });
-                            }
-                        }
-                        else
-                        {
-                            for (int v = 0; v < vertCount; v++)
-                            {
-                                var indices = sr.ReadBytes(sr.Position, 4);
-                                vertWeightIndices.Add(new int[] { indices[0], indices[1], indices[2], indices[3] });
-                                sr.Seek(4, System.IO.SeekOrigin.Current);
-                            }
-                        }
-                        break;
-                    case VertexMagic.BONW:
-                        for (int v = 0; v < vertCount; v++)
-                        {
-                            var weights = sr.ReadBytes(sr.Position, 4);
-                            vertWeights.Add(new Vector4((float)((double)weights[0] / 0xFF), (float)((double)weights[1] / 0xFF), (float)((double)weights[2] / 0xFF), (float)((double)weights[3] / 0xFF)));
-                            sr.Seek(4, System.IO.SeekOrigin.Current);
-                        }
-                        break;
-                    case VertexMagic.SAT_:
-                        unkDict.Add(vertDefs[i].dataMagic, sr.ReadBytes(sr.Position, vertDefs[i].dataSize));
-                        sr.Seek(vertexDataStart + vertDefs[i].dataStart + vertDefs[i].dataSize, System.IO.SeekOrigin.Begin);
-                        break;
-                    default:
-                        Debug.WriteLine($"Unknown data type {vertDefs[i].dataMagic.ToString("X")} {vertDefs[i].dataMagic} {UTF8Encoding.UTF8.GetString(BitConverter.GetBytes((int)vertDefs[i].dataMagic))}");
-                        unkDict.Add(vertDefs[i].dataMagic, sr.ReadBytes(sr.Position, vertDefs[i].dataSize));
-                        sr.Seek(vertexDataStart + vertDefs[i].dataStart + vertDefs[i].dataSize, System.IO.SeekOrigin.Begin);
-                        break;
-                }
+                    }
+                    break;
+                case VertexMagic.BONW:
+                    for (int v = 0; v < vertCount; v++)
+                    {
+                        var weights = sr.ReadBytes(sr.Position, 4);
+                        vertWeights.Add(new Vector4((float)((double)weights[0] / 0xFF), (float)((double)weights[1] / 0xFF), (float)((double)weights[2] / 0xFF), (float)((double)weights[3] / 0xFF)));
+                        sr.Seek(4, System.IO.SeekOrigin.Current);
+                    }
+                    break;
+                case VertexMagic.SAT_:
+                    unkDict.Add(dataMagic, sr.ReadBytes(sr.Position,(int)dataSize));
+                    break;
+                default:
+                    Debug.WriteLine($"Unknown data type {dataMagic.ToString("X")} {dataMagic} {UTF8Encoding.UTF8.GetString(BitConverter.GetBytes((int)dataMagic))}");
+                    unkDict.Add(dataMagic, sr.ReadBytes(sr.Position, (int)dataSize));
+                    break;
             }
         }
 
