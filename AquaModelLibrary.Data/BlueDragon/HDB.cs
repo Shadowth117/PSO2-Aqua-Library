6b93e7c69c0e89793fe3b06a70374998cbfc497e
Shadow <shadic@msn.com>
2024-05-31 22:03:28 -0400

    Blue Dragon HDB Model wip

---
+++ AquaModelLibrary.Data/BlueDragon/HDB.cs
@@ -0,0 +1,28 @@
+﻿namespace AquaModelLibrary.Data.BlueDragon
+{
+    public class HDB
+    {
+        public int magic;
+        /// <summary>
+        /// Always 0x10000
+        /// </summary>
+        public int int_04;
+        public int fileSize;
+        /// <summary>
+        /// Always 0x4?
+        /// </summary>
+        public int int_0C;
+
+        public int int_10;
+        /// <summary>
+        /// Always 0xC?
+        /// </summary>
+        public int int_14;
+        public int int_18;
+        public int int_1C;
+
+        //Counts number of 0x10 length rows with data pointers 
+        public int dataRowCount;
+
+    }
+}
