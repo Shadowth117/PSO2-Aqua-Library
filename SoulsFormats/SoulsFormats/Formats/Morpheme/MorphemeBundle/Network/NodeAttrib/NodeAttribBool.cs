3603e6287c15077066e98edd0dd5cbb6724d7eb0
Shadow <shadic@msn.com>
2024-07-23 21:09:47 -0400

    Switch to SoulsFormatsNext Submodule

--- SoulsFormats/SoulsFormats/Formats/Morpheme/MorphemeBundle/Network/NodeAttrib/NodeAttribBool.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SoulsFormats.Formats.Morpheme.MorphemeBundle.Network.NodeAttrib
-{
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-    public class NodeAttribBool : NodeAttribBase
-    {
-        public bool boolVar;
-    }
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
-}
