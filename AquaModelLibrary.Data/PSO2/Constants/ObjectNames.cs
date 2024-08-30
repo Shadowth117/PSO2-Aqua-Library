516ce89a7e745f1b665dc950677e2374a02452b2
Shadow <shadic@msn.com>
2024-08-30 03:20:59 -0400

    Add Godzilla Lobby object names

--- AquaModelLibrary.Data/PSO2/Constants/ObjectNames.cs
+++ AquaModelLibrary.Data/PSO2/Constants/ObjectNames.cs
@@ -8,7 +8,8 @@
 
         public static Dictionary<string, string> classicObjectNames = new Dictionary<string, string>()
         {
-
+            { "ob_01000_0152", "ロビー・ゴジラ,Lobby Godzilla" },
+            { "ob_01000_0153", ",Lobby Godzilla Stand" },
         };
 
         public static Dictionary<string, string> rebootObjectNames = new Dictionary<string, string>()
