eccad242a9d79d2030e37eed0ae685c60c587fc0
Shadow <shadic@msn.com>
2024-08-31 20:09:15 -0400

    Get ninja specific things out of the GC enums

--- AquaModelLibrary.Data/Gamecube/GCEnums.cs
+++ AquaModelLibrary.Data/Gamecube/GCEnums.cs
@@ -1,6 +1,4 @@
 ﻿//Sourced from SA Tools
-using AquaModelLibrary.Data.Ninja;
-
 namespace AquaModelLibrary.Data.Gamecube
 {
     /// <summary>
@@ -393,63 +391,6 @@ namespace AquaModelLibrary.Data.Gamecube
         Scale7 = 0xE,       // UV * 0.015625
         Scale8 = 0xF,       // UV * 0.0078125
         Scale9 = 0x10,      // Same as NoUV1
-
-    }
-
-    /// <summary>
-    /// Used to convert between NJ and GC enums
-    /// </summary>
-    public static class GCEnumConverter
-    {
-        public static AlphaInstruction GXToNJAlphaInstruction(GCBlendModeControl gx)
-        {
-            switch (gx)
-            {
-                case GCBlendModeControl.SrcAlpha:
-                    return AlphaInstruction.SourceAlpha;
-                case GCBlendModeControl.DstAlpha:
-                    return AlphaInstruction.DestinationAlpha;
-                case GCBlendModeControl.InverseSrcAlpha:
-                    return AlphaInstruction.InverseSourceAlpha;
-                case GCBlendModeControl.InverseDstAlpha:
-                    return AlphaInstruction.InverseDestinationAlpha;
-                case GCBlendModeControl.SrcColor:
-                    return AlphaInstruction.OtherColor;
-                case GCBlendModeControl.InverseSrcColor:
-                    return AlphaInstruction.InverseOtherColor;
-                case GCBlendModeControl.One:
-                    return AlphaInstruction.One;
-                case GCBlendModeControl.Zero:
-                    return AlphaInstruction.Zero;
-            }
-
-            return AlphaInstruction.Zero;
-        }
-
-        public static GCBlendModeControl NJtoGXBlendModeControl(AlphaInstruction nj)
-        {
-            switch (nj)
-            {
-                case AlphaInstruction.SourceAlpha:
-                    return GCBlendModeControl.SrcAlpha;
-                case AlphaInstruction.DestinationAlpha:
-                    return GCBlendModeControl.DstAlpha;
-                case AlphaInstruction.InverseSourceAlpha:
-                    return GCBlendModeControl.InverseSrcAlpha;
-                case AlphaInstruction.InverseDestinationAlpha:
-                    return GCBlendModeControl.InverseDstAlpha;
-                case AlphaInstruction.OtherColor:
-                    return GCBlendModeControl.SrcColor;
-                case AlphaInstruction.InverseOtherColor:
-                    return GCBlendModeControl.InverseSrcColor;
-                case AlphaInstruction.One:
-                    return GCBlendModeControl.One;
-                case AlphaInstruction.Zero:
-                    return GCBlendModeControl.Zero;
-            }
-
-            return GCBlendModeControl.Zero;
-        }
 
     }
 
---
+++ AquaModelLibrary.Data/Ninja/Model/Ginja/GinjaEnumConverter.cs
@@ -0,0 +1,61 @@
+﻿using AquaModelLibrary.Data.Gamecube;
+
+namespace AquaModelLibrary.Data.Ninja.Model.Ginja
+{
+    /// <summary>
+    /// Used to convert between NJ and GC enums
+    /// </summary>
+    public static class GCEnumConverter
+    {
+        public static AlphaInstruction GXToNJAlphaInstruction(GCBlendModeControl gx)
+        {
+            switch (gx)
+            {
+                case GCBlendModeControl.SrcAlpha:
+                    return AlphaInstruction.SourceAlpha;
+                case GCBlendModeControl.DstAlpha:
+                    return AlphaInstruction.DestinationAlpha;
+                case GCBlendModeControl.InverseSrcAlpha:
+                    return AlphaInstruction.InverseSourceAlpha;
+                case GCBlendModeControl.InverseDstAlpha:
+                    return AlphaInstruction.InverseDestinationAlpha;
+                case GCBlendModeControl.SrcColor:
+                    return AlphaInstruction.OtherColor;
+                case GCBlendModeControl.InverseSrcColor:
+                    return AlphaInstruction.InverseOtherColor;
+                case GCBlendModeControl.One:
+                    return AlphaInstruction.One;
+                case GCBlendModeControl.Zero:
+                    return AlphaInstruction.Zero;
+            }
+
+            return AlphaInstruction.Zero;
+        }
+
+        public static GCBlendModeControl NJtoGXBlendModeControl(AlphaInstruction nj)
+        {
+            switch (nj)
+            {
+                case AlphaInstruction.SourceAlpha:
+                    return GCBlendModeControl.SrcAlpha;
+                case AlphaInstruction.DestinationAlpha:
+                    return GCBlendModeControl.DstAlpha;
+                case AlphaInstruction.InverseSourceAlpha:
+                    return GCBlendModeControl.InverseSrcAlpha;
+                case AlphaInstruction.InverseDestinationAlpha:
+                    return GCBlendModeControl.InverseDstAlpha;
+                case AlphaInstruction.OtherColor:
+                    return GCBlendModeControl.SrcColor;
+                case AlphaInstruction.InverseOtherColor:
+                    return GCBlendModeControl.InverseSrcColor;
+                case AlphaInstruction.One:
+                    return GCBlendModeControl.One;
+                case AlphaInstruction.Zero:
+                    return GCBlendModeControl.Zero;
+            }
+
+            return GCBlendModeControl.Zero;
+        }
+
+    }
+}
