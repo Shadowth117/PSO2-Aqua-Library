47e18d142b114f6c360747b1f910af71cdb310fe
Shadow <shadic@msn.com>
2024-11-14 16:55:58 -0500

    Update Unluac to .net6 to match other projects since godot threw a fit

--- UnluacNET/Properties/AssemblyInfo.cs
+++ UnluacNET/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Unluac .NET")]
-[assembly: AssemblyDescription("Lua 5.1 decompiler based on 'unluac' by tehtmi.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Unluac .NET")]
-[assembly: AssemblyCopyright("Copyright © 2014 Mark Ludwig, tehtmi")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("de89349b-326c-4299-b15a-494f7533a59d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
