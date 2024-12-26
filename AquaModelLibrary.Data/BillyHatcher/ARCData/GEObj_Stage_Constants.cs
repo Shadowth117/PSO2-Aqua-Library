498381dcc864865287b292526ee83791e1a1123d
Shadow <shadic@msn.com>
2024-12-26 05:17:44 -0500

    Preliminary billy GeobjStage texture list mapping

---
+++ AquaModelLibrary.Data/BillyHatcher/ARCData/GEObj_Stage_Constants.cs
@@ -0,0 +1,133 @@
+﻿namespace AquaModelLibrary.Data.BillyHatcher.ARCData
+{
+    public class GEObj_Stage_Constants
+    {
+        //Tex list maps are for asset list 1. Asset list 2 always uses the same texture list for everything
+        public static Dictionary<string, string> texListCommon = new Dictionary<string, string>()
+        {
+            { "MODEL_DOOR01", "TLS_MODE_DOOR01" },
+            { "MODEL_MON01", "TLS_MODEL_MON01" },
+            { "MODEL_TURIBASHI_HASHIRA01", "TLS_MODEL_TURIBASHI_HASHIRA01"},
+            { "MODEL_TURIBASHI_HASHIRA02", "TLS_MODEL_TURIBASHI_HASHIRA02"},
+            { "MODEL_TURIBASHI_ITA01", "TLS_MODEL_TURIBASHI_ITA01"},
+            { "MODEL_TURIBASHI_ITA02", "TLS_MODEL_TURIBASHI_ITA02"},
+            { "MODEL_TURIBASHI_ITA03", "TLS_MODEL_TURIBASHI_ITA03"},
+            { "MODEL_TURIBASHI_ROPE", "TLS_MODEL_TURIBASHI_ROPE"},
+            { "MODEL_STAND", "TLS_MODEL_STAND"},
+            { "MODEL_CHICKEN_BOSS", "TLS_MODEL_CHICKEN_BOSS"},
+            { "MODEL_PANEL_FLOOR", "TLS_MODEL_PANEL_FLOOR"},
+            { "MODEL_PANEL_INDICATOR", "TLS_MODEL_PANEL_INDICATOR"},
+            { "MODEL_PANEL", "TLS_MODEL_PANEL"},
+            { "MODEL_BATTERY", "TLS_MODEL_BATTERY"},
+            { "MODEL_PANEL_FLOOR", "TLS_MODEL_PANEL_FLOOR"},
+            { "MODEL_ICE_FENCE_D", "TLS_MODEL_ICE_FENCE_D"},
+            { "MODEL_ICE_WALL", "TLS_MODEL_ICE_WALL"},
+            { "MODEL_ICE_FLOOR", "TLS_MODEL_ICE_FLOOR"},
+            { "MODEL_GEAR_L", "TLS_MODEL_GEAR_L"},
+            { "MODEL_GEAR_S", "TLS_MODEL_GEAR_S"},
+            { "MODEL_ICE_GEAR_L", "TLS_MODEL_ICE_GEAR_L"},
+            { "MODEL_ICE_GEAR_S", "TLS_MODEL_ICE_GEAR_S"},
+            { "MODEL_PROPELLER", "TLS_MODEL_PROPELLER"},
+        };
+
+        public static Dictionary<string, string> texListMapBlack = new Dictionary<string, string>() {
+            { "model_0", "texList_0" },
+            { "model_1", "texList_1" },
+            { "model_2", "texList_2" }
+        };
+
+        public static Dictionary<string, string> texListMapBlue = new Dictionary<string, string>() {
+            { "model_0", "texList_0" },
+            { "model_1", "texList_1" },
+            { "model_2", "texList_2" },
+            { "model_3", "texList_3" },
+            { "model_4", "texList_4" },
+            { "model_5", "texList_5" },
+            { "model_14", "texList_14" },
+            { "model_15", "texList_15" },
+            { "model_16", "texList_15" },
+            { "model_17", "texList_15" },
+            { "model_18", "texList_16" },
+            { "model_19", "texList_17" },
+            { "model_20", "texList_18" },
+            { "model_21", "texList_19" },
+            { "model_22", "texList_19" },
+        };
+
+
+        public static Dictionary<string, string> texListMapGreen = new Dictionary<string, string>() {
+            { "model_0", "texList_0" },
+            { "model_1", "texList_1" },
+            { "model_2", "texList_2" },
+            { "model_3", "texList_3" },
+            { "model_4", "texList_4" },
+            { "model_5", "texList_5" },
+        };
+
+        public static Dictionary<string, string> texListMapOrange = new Dictionary<string, string>() {
+            { "model_0", "texList_0" },
+            { "model_1", "texList_1" },
+            { "model_2", "texList_2" },
+            { "model_3", "texList_3" },
+            { "model_4", "texList_4" },
+            { "model_5", "texList_5" },
+            { "model_6", "texList_6" },
+            { "model_7", "texList_7" },
+            { "model_8", "texList_8" },
+            { "model_9", "texList_9" },
+            { "model_10", "texList_10" },
+        };
+
+        public static Dictionary<string, string> texListMapPurple = new Dictionary<string, string>() {
+            { "model_0", "texList_0" },
+            { "model_1", "texList_0" },
+            { "model_2", "texList_0" },
+            { "model_3", "texList_0" },
+            { "model_4", "texList_1" },
+            { "model_5", "texList_2" },
+        };
+
+        public static Dictionary<string, string> texListMapRed = new Dictionary<string, string>() {
+            { "model_0", "texList_0" },
+            { "model_1", "texList_1" },
+            { "model_2", "texList_2" },
+            { "model_3", "texList_3" },
+            { "model_4", "texList_4" },
+            { "model_5", "texList_5" },
+            { "model_6", "texList_6" },
+            { "model_7", "texList_7" },
+            { "model_8", "texList_8" },
+            { "model_9", "texList_9" },
+            { "model_10", "texList_9" },
+            { "model_11", "texList_9" },
+            { "model_12", "texList_9" },
+            { "model_13", "texList_9" },
+            { "model_14", "texList_9" },
+            { "model_15", "texList_9" },
+            { "model_16", "texList_9" },
+            { "model_17", "texList_9" },
+            { "model_18", "texList_9" },
+            { "model_19", "texList_10" },
+        };
+
+        public static Dictionary<string, string> texListMapYellow = new Dictionary<string, string>() {
+            { "model_0", "texList_0" },
+            { "model_1", "texList_1" },
+            { "model_2", "texList_2" },
+            { "model_3", "texList_3" },
+            { "model_4", "texList_4" },
+            { "model_5", "texList_5" },
+            { "model_6", "texList_6" },
+            { "model_7", "texList_7" },
+            { "model_8", "texList_8" },
+            { "model_9", "texList_9" },
+            { "model_10", "texList_10" },
+            { "model_11", "texList_11" },
+            { "model_12", "texList_12" },
+            { "model_13", "texList_13" },
+            { "model_14", "texList_14" },
+            { "model_15", "texList_15" },
+            { "model_16", "texList_16" },
+        };
+    }
+}
