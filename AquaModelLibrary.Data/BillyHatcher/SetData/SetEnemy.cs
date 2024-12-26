f4621a6676c0dcc938c7d1c41e91966f0db8c6fe
Shadow <shadic@msn.com>
2024-12-25 22:58:00 -0500

    Set Enemy WIP

--- AquaModelLibrary.Data/BillyHatcher/SetData/SetEnemy.cs
+++ AquaModelLibrary.Data/BillyHatcher/SetData/SetEnemy.cs
@@ -5,6 +5,10 @@ using System.Numerics;
 
 namespace AquaModelLibrary.Data.BillyHatcher.SetData
 {
+    /// <summary>
+    /// There are 2 versions of this. One has 0x50 long structures, one has 0x3C long structures. The latter isn't intentionally used for any final areas, though green_boss uses this layout. 
+    /// However since green_boss has the boss first, the underwrite doesn't matter since the 2nd object's 0x14 of data doesn't affect the boss, seemingly.
+    /// </summary>
     public class SetEnemyList
     {
         public List<SetEnemy> setEnemies = new List<SetEnemy>();
@@ -32,19 +36,19 @@ namespace AquaModelLibrary.Data.BillyHatcher.SetData
                 setEnemy.enemyId = sr.ReadBE<int>();
                 setEnemy.Position = sr.ReadBEV3();
                 setEnemy.BAMSRotation = new Vector3Int.Vec3Int(sr.ReadBE<int>(), sr.ReadBE<int>(), sr.ReadBE<int>());
+                setEnemy.int_1C = sr.ReadBE<int>();
+                setEnemy.int_20 = sr.ReadBE<int>();
+                setEnemy.int_24 = sr.ReadBE<int>();
+                setEnemy.int_28 = sr.ReadBE<int>();
                 setEnemy.int_2C = sr.ReadBE<int>();
                 setEnemy.int_30 = sr.ReadBE<int>();
                 setEnemy.int_34 = sr.ReadBE<int>();
                 setEnemy.int_38 = sr.ReadBE<int>();
                 setEnemy.int_3C = sr.ReadBE<int>();
-                setEnemy.int_40 = sr.ReadBE<int>();
-                setEnemy.int_44 = sr.ReadBE<int>();
-                setEnemy.int_48 = sr.ReadBE<int>();
-                setEnemy.int_4C = sr.ReadBE<int>();
-                setEnemy.int_50 = sr.ReadBE<int>();
-                setEnemy.flt_54 = sr.ReadBE<float>();
-                setEnemy.flt_58 = sr.ReadBE<float>();
-                setEnemy.flt_5C = sr.ReadBE<float>();
+                setEnemy.flt_40 = sr.ReadBE<float>();
+                setEnemy.flt_44 = sr.ReadBE<float>();
+                setEnemy.flt_48 = sr.ReadBE<float>();
+                setEnemy.flt_4C = sr.ReadBE<float>();
                 setEnemies.Add(setEnemy);
             }
         }
@@ -60,19 +64,19 @@ namespace AquaModelLibrary.Data.BillyHatcher.SetData
                 outBytes.AddValue(setEne.BAMSRotation.X);
                 outBytes.AddValue(setEne.BAMSRotation.Y);
                 outBytes.AddValue(setEne.BAMSRotation.Z);
+                outBytes.AddValue(setEne.int_1C);
+                outBytes.AddValue(setEne.int_20);
+                outBytes.AddValue(setEne.int_24);
+                outBytes.AddValue(setEne.int_28);
                 outBytes.AddValue(setEne.int_2C);
                 outBytes.AddValue(setEne.int_30);
                 outBytes.AddValue(setEne.int_34);
                 outBytes.AddValue(setEne.int_38);
                 outBytes.AddValue(setEne.int_3C);
-                outBytes.AddValue(setEne.int_40);
-                outBytes.AddValue(setEne.int_44);
-                outBytes.AddValue(setEne.int_48);
-                outBytes.AddValue(setEne.int_4C);
-                outBytes.AddValue(setEne.int_50);
-                outBytes.AddValue(setEne.flt_54);
-                outBytes.AddValue(setEne.flt_58);
-                outBytes.AddValue(setEne.flt_5C);
+                outBytes.AddValue(setEne.flt_40);
+                outBytes.AddValue(setEne.flt_44);
+                outBytes.AddValue(setEne.flt_48);
+                outBytes.AddValue(setEne.flt_4C);
             }
 
             ByteListExtension.Reset();
@@ -85,6 +89,11 @@ namespace AquaModelLibrary.Data.BillyHatcher.SetData
         public int enemyId;
         public Vector3 Position;
         public Vector3Int.Vec3Int BAMSRotation;
+        public int int_1C;
+
+        public int int_20;
+        public int int_24;
+        public int int_28;
         public int int_2C;
 
         public int int_30;
@@ -92,14 +101,9 @@ namespace AquaModelLibrary.Data.BillyHatcher.SetData
         public int int_38;
         public int int_3C;
 
-        public int int_40;
-        public int int_44;
-        public int int_48;
-        public int int_4C;
-
-        public int int_50;
-        public float flt_54;
-        public float flt_58;
-        public float flt_5C;
+        public float flt_40;
+        public float flt_44;
+        public float flt_48;
+        public float flt_4C;
     }
 }
