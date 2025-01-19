f139a29cb8b793b80ff8624b00603a5531e7fee5
Shadow <shadic@msn.com>
2025-01-19 00:24:57 -0500

    Add enemy filenames

--- AquaModelLibrary.Data/BillyHatcher/ARCData/ArEnemy.cs
+++ AquaModelLibrary.Data/BillyHatcher/ARCData/ArEnemy.cs
@@ -8,9 +8,75 @@ namespace AquaModelLibrary.Data.BillyHatcher.ARCData
 {
     /// <summary>
     /// ar_ene_*.arc - These contain data for enemy models, motions, and a texture list. The GVM is stored externally with the same name, but no preceding ar_ and the .gvm extension
+    /// ene_am04 is unused in final, but present on PC
     /// </summary>
     public class ArEnemy : ARC
     {
+        public static string[] archiveFilenames = new string[] { 
+            "ar_ene_am02.arc",
+            "ar_ene_am04.arc",
+            "ar_ene_ants_queen.arc",
+            "ar_ene_armadillo.arc",
+            "ar_ene_bat.arc",
+            "ar_ene_blue_boss.arc",
+            "ar_ene_common.arc",
+            "ar_ene_frog.arc",
+            "ar_ene_green_boss.arc",
+            "ar_ene_green_demo.arc",
+            "ar_ene_last_boss.arc",
+            "ar_ene_last_dark.arc",
+            "ar_ene_last_ex_boss.arc",
+            "ar_ene_lizard.arc",
+            "ar_ene_orange_boss.arc",
+            "ar_ene_orange_boss_map.arc",
+            "ar_ene_penguin.arc",
+            "ar_ene_purple_boss.arc",
+            "ar_ene_red_boss.arc",
+            "ar_ene_snake.arc",
+            "ar_ene_yellow_boss.arc",
+            "ar_ene_yellow_boss_green.arc",
+            "ar_ene_zako_ant.arc",
+            "ar_ene_zako_bat.arc",
+            "ar_ene_zako_cat.arc",
+            "ar_ene_zako_fugu.arc",
+            "ar_ene_zako_mtn.arc",
+            "ar_ene_zako_snake.arc",
+        };
+
+        public static string[] textureFilenames = new string[] {
+            "ene_am02.GVM",
+            "ene_am04.GVM",
+            "ene_ants_queen.GVM",
+            "ene_armadillo.GVM",
+            "ene_bat.GVM",
+            "ene_blue_boss.GVM",
+            "ene_blue_boss_suimen.GVM",
+            "ene_common.GVM",
+            "ene_emotion.GVM",
+            "ene_eye.GVM",
+            "ene_frog.GVM",
+            "ene_green_boss.GVM",
+            "ene_green_demo.GVM",
+            "ene_last_boss.GVM",
+            "ene_last_dark.GVM",
+            "ene_last_ex_boss.GVM",
+            "ene_lizard.GVM",
+            "ene_orange_boss.GVM",
+            "ene_orange_boss_map.GVM",
+            "ene_penguin.GVM",
+            "ene_purple_boss.GVM",
+            "ene_red_boss.GVM",
+            "ene_snake.GVM",
+            "ene_yellow_boss.GVM",
+            "ene_yellow_boss_green.GVM",
+            "ene_zako_ant.GVM",
+            "ene_zako_bat.GVM",
+            "ene_zako_cat.GVM",
+            "ene_zako_fugu.GVM",
+            "ene_zako_mtn.GVM",
+            "ene_zako_snake.GVM",
+        };
+
         public List<NJSObject> models = new List<NJSObject>();
         public List<List<NJSMotion>> anims = new List<List<NJSMotion>>();
         public List<NJTextureList> texList = new List<NJTextureList>();
