54d77be909674035fce6a4b77585cd9a205d32c9
Shadow <shadic@msn.com>
2025-06-21 02:40:00 -0400

    Fix for trial animodels

--- AquaModelLibrary.Data/BillyHatcher/ARCData/AniModel.cs
+++ AquaModelLibrary.Data/BillyHatcher/ARCData/AniModel.cs
@@ -54,103 +54,13 @@ namespace AquaModelLibrary.Data.BillyHatcher.ARCData
                 sr.Seek(0x20 + group1FileReferences[i].fileOffset, SeekOrigin.Begin);
                 switch (fileName)
                 {
+                    //Technically this just points to the actual GJ model, but the file layout is still the same as modern ani_model, so we can go there even though it's not technically pointed to
+                    case "MODEL_BODY":
+                        sr.Seek(0x20, SeekOrigin.Begin);
+                        ReadAniModel(sr);
+                        break;
                     case "AniModel":
-                        var modelAddress = sr.ReadBE<int>();
-                        var animAddress = sr.ReadBE<int>();
-                        var boundingAddress = sr.ReadBE<int>();
-                        var anim2Address = sr.ReadBE<int>();
-
-                        var unkFloatAddress = sr.ReadBE<int>();
-                        var texListAddress = sr.ReadBE<int>();
-                        var gvmAddress = sr.ReadBE<int>();
-
-                        //Read models
-                        var modelCount = (animAddress - modelAddress) / 4;
-                        List<int> modelAddresses = new List<int>();
-                        sr.Seek(0x20 + modelAddress, SeekOrigin.Begin);
-                        for(int j = 0; j < modelCount; j++)
-                        {
-                            modelAddresses.Add(sr.ReadBE<int>());
-                        }
-                        foreach(var modelAddr in modelAddresses)
-                        {
-                            sr.Seek(0x20 + modelAddr, SeekOrigin.Begin);
-                            models.Add(new NJSObject(sr, NinjaVariant.Ginja, sr._BEReadActive, 0x20));
-                        }
-
-                        //Read Animations
-                        var animCount = (boundingAddress - animAddress) / 4;
-                        List<int> animAddresses = new List<int>();
-                        sr.Seek(0x20 + animAddress, SeekOrigin.Begin);
-                        for (int j = 0; j < animCount; j++)
-                        {
-                            animAddresses.Add(sr.ReadBE<int>());
-                        }
-                        foreach (var animAddr in animAddresses)
-                        {
-                            if(animAddr != -1)
-                            {
-                                sr.Seek(0x20 + animAddr, SeekOrigin.Begin);
-                                motions.Add(new NJSMotion(sr, true, 0x20));
-                            } else
-                            {
-                                motions.Add(null);
-                            }
-                        }
-
-                        //Read Bounding
-                        var boundCount = (anim2Address - boundingAddress) / 4;
-                        List<int> boundAddresses = new List<int>();
-                        sr.Seek(0x20 + boundingAddress, SeekOrigin.Begin);
-                        for (int j = 0; j < boundCount; j++)
-                        {
-                            boundAddresses.Add(sr.ReadBE<int>());
-                        }
-                        foreach (var boundAddr in boundAddresses)
-                        {
-                            sr.Seek(0x20 + boundAddr, SeekOrigin.Begin);
-                            cameraAnchorList.Add(new CameraAnchor() { Min = sr.ReadBEV3(), Max = sr.ReadBEV3(), int_18 = sr.ReadBE<int>(), int_1C = sr.ReadBE<int>()});
-                        }
-
-                        //Read Animation 2s
-                        var nextAddress = unkFloatAddress > 0 ? unkFloatAddress : texListAddress;
-                        var anim2Count = (nextAddress - anim2Address) / 4;
-                        List<int> anim2Addresses = new List<int>();
-                        sr.Seek(0x20 + anim2Address, SeekOrigin.Begin);
-                        for (int j = 0; j < anim2Count; j++)
-                        {
-                            anim2Addresses.Add(sr.ReadBE<int>());
-                        }
-                        foreach (var animAddr in anim2Addresses)
-                        {
-                            sr.Seek(0x20 + animAddr, SeekOrigin.Begin);
-                            motion2s.Add(new NJSMotion(sr, true, 0x20));
-                        }
-
-                        //Read Weird Floats
-                        if(unkFloatAddress != 0)
-                        {
-                            var floatCount = (texListAddress - unkFloatAddress) / 4;
-                            List<int> floatAddresses = new List<int>();
-                            sr.Seek(0x20 + unkFloatAddress, SeekOrigin.Begin);
-                            for (int j = 0; j < floatCount; j++)
-                            {
-                                floatAddresses.Add(sr.ReadBE<int>());
-                            }
-                            foreach (var floatAddr in floatAddresses)
-                            {
-                                sr.Seek(0x20 + floatAddr, SeekOrigin.Begin);
-                                unkFloatList.Add(sr.ReadBE<float>());
-                            }
-                        }
-
-                        //Read Texture data
-                        sr.Seek(0x20 + texListAddress, SeekOrigin.Begin);
-                        sr.Seek(0x20 + sr.ReadBE<int>(), SeekOrigin.Begin);
-                        texList = new NJTextureList(sr, 0x20);
-
-                        sr.Seek(0x20 + gvmAddress, SeekOrigin.Begin);
-                        gvm = new PuyoFile(GVMUtil.ReadGVMBytes(sr));
+                        ReadAniModel(sr);
                         break;
                     case "polyanim":
                         polyAnim = new PolyAnim(sr, 0x20);
@@ -160,6 +70,115 @@ namespace AquaModelLibrary.Data.BillyHatcher.ARCData
                         break;
                 }
             }
+        }
+
+        private void ReadAniModel(BufferedStreamReaderBE<MemoryStream> sr)
+        {
+            var modelAddress = sr.ReadBE<int>();
+            var animAddress = sr.ReadBE<int>();
+            var boundingAddress = sr.ReadBE<int>();
+            var anim2Address = sr.ReadBE<int>();
+
+            var unkFloatAddress = sr.ReadBE<int>();
+            var texListAddress = sr.ReadBE<int>();
+            var gvmAddress = sr.ReadBE<int>();
+
+            //In the trial there's a file that a null anim, but the only way to notice this is to check the next offset and see if it's the same
+            int animSanityCheck = -1;
+            if(boundingAddress != 0)
+            {
+                sr.Seek(0x20 + boundingAddress, SeekOrigin.Begin);
+                animSanityCheck = sr.ReadBE<int>();
+            }
+
+            //Read models
+            var modelCount = (animAddress - modelAddress) / 4;
+            List<int> modelAddresses = new List<int>();
+            sr.Seek(0x20 + modelAddress, SeekOrigin.Begin);
+            for (int j = 0; j < modelCount; j++)
+            {
+                modelAddresses.Add(sr.ReadBE<int>());
+            }
+            foreach (var modelAddr in modelAddresses)
+            {
+                sr.Seek(0x20 + modelAddr, SeekOrigin.Begin);
+                models.Add(new NJSObject(sr, NinjaVariant.Ginja, sr._BEReadActive, 0x20));
+            }
+
+            //Read Animations
+            var animCount = (boundingAddress - animAddress) / 4;
+            List<int> animAddresses = new List<int>();
+            sr.Seek(0x20 + animAddress, SeekOrigin.Begin);
+            for (int j = 0; j < animCount; j++)
+            {
+                animAddresses.Add(sr.ReadBE<int>());
+            }
+            foreach (var animAddr in animAddresses)
+            {
+                if (animAddr != -1 && animAddr != animSanityCheck)
+                {
+                    sr.Seek(0x20 + animAddr, SeekOrigin.Begin);
+                    motions.Add(new NJSMotion(sr, true, 0x20));
+                }
+                else
+                {
+                    motions.Add(null);
+                }
+            }
+
+            //Read Bounding
+            var boundCount = (anim2Address - boundingAddress) / 4;
+            List<int> boundAddresses = new List<int>();
+            sr.Seek(0x20 + boundingAddress, SeekOrigin.Begin);
+            for (int j = 0; j < boundCount; j++)
+            {
+                boundAddresses.Add(sr.ReadBE<int>());
+            }
+            foreach (var boundAddr in boundAddresses)
+            {
+                sr.Seek(0x20 + boundAddr, SeekOrigin.Begin);
+                cameraAnchorList.Add(new CameraAnchor() { Min = sr.ReadBEV3(), Max = sr.ReadBEV3(), int_18 = sr.ReadBE<int>(), int_1C = sr.ReadBE<int>() });
+            }
+
+            //Read Animation 2s
+            var nextAddress = unkFloatAddress > 0 ? unkFloatAddress : texListAddress;
+            var anim2Count = (nextAddress - anim2Address) / 4;
+            List<int> anim2Addresses = new List<int>();
+            sr.Seek(0x20 + anim2Address, SeekOrigin.Begin);
+            for (int j = 0; j < anim2Count; j++)
+            {
+                anim2Addresses.Add(sr.ReadBE<int>());
+            }
+            foreach (var animAddr in anim2Addresses)
+            {
+                sr.Seek(0x20 + animAddr, SeekOrigin.Begin);
+                motion2s.Add(new NJSMotion(sr, true, 0x20));
+            }
+
+            //Read Weird Floats
+            if (unkFloatAddress != 0)
+            {
+                var floatCount = (texListAddress - unkFloatAddress) / 4;
+                List<int> floatAddresses = new List<int>();
+                sr.Seek(0x20 + unkFloatAddress, SeekOrigin.Begin);
+                for (int j = 0; j < floatCount; j++)
+                {
+                    floatAddresses.Add(sr.ReadBE<int>());
+                }
+                foreach (var floatAddr in floatAddresses)
+                {
+                    sr.Seek(0x20 + floatAddr, SeekOrigin.Begin);
+                    unkFloatList.Add(sr.ReadBE<float>());
+                }
+            }
+
+            //Read Texture data
+            sr.Seek(0x20 + texListAddress, SeekOrigin.Begin);
+            sr.Seek(0x20 + sr.ReadBE<int>(), SeekOrigin.Begin);
+            texList = new NJTextureList(sr, 0x20);
+
+            sr.Seek(0x20 + gvmAddress, SeekOrigin.Begin);
+            gvm = new PuyoFile(GVMUtil.ReadGVMBytes(sr));
         }
 
         public byte[] GetBytes(bool crabSharkOrder = false)
