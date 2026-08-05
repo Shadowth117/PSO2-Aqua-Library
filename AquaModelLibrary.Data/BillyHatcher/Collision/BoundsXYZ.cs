63e952a10f01539dc92e48f8f636cb2fe9a2fa51
Shadowth117 <shadic@msn.com>
2026-08-04 23:16:44 -0700

    Add missing bounds data

--- AquaModelLibrary.Data/BillyHatcher/Collision/BoundsXYZ.cs
+++ AquaModelLibrary.Data/BillyHatcher/Collision/BoundsXYZ.cs
@@ -6,5 +6,7 @@ namespace AquaModelLibrary.Data.BillyHatcher.Collision
     {
         public Vector3 Min;
         public Vector3 Max;
+        public int int_18;
+        public int int_1C;
     }
 }
