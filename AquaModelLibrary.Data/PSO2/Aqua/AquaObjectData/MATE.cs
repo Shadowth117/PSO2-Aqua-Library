e8a5e80d75eb232c4cfe484a1642586581a67813
Shadowth117 <shadic@msn.com>
2026-08-27 19:47:49 -0700

    Kill unused using

--- AquaModelLibrary.Data/PSO2/Aqua/AquaObjectData/MATE.cs
+++ AquaModelLibrary.Data/PSO2/Aqua/AquaObjectData/MATE.cs
@@ -1,6 +1,5 @@
 ﻿using AquaModelLibrary.Data.DataTypes.SetLengthStrings;
 using AquaModelLibrary.Helpers.MathHelpers;
-using System;
 using System.Numerics;
 
 namespace AquaModelLibrary.Data.PSO2.Aqua.AquaObjectData
